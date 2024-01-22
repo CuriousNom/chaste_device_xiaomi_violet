7a444276e58726fe1cd266d7500f123f88a07c0f
CuriousNom <aryanbiswas7585@gmail.com>
2024-01-23 00:56:15 +0530

    violet: enable background blur

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -19,6 +19,7 @@ MATRIXX_MAINTAINER := Aryan
 MATRIXX_CHIPSET := SM6150
 MATRIXX_BATTERY := 4000mAh
 MATRIXX_DISPLAY := 1080x2340
+TARGET_ENABLE_BLUR := true
 WITH_GMS := false
 
 # Device identifier. This must come after all inclusions.
